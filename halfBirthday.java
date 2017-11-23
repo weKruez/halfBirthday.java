e4d63d5299797e230d7f5e19a1e1159a5d56e2d7
Christian Weiss <w.christianastor@gmail.com>
2017-11-23 13:30:14 -0500

    comments added

--- halfBirthday.java
+++ halfBirthday.java
@@ -1,4 +1,4 @@
-/*could not convert day into double for accurate algorithm. leap year is -183, common year is -182.5* So rounded to all dates 183.*/
+/*could not convert day into double for accurate algorithm. leap year is -183, common year is -182.5* So rounded to all dates 183. Common years not taken into account. Slight errors. */
 
 import java.time.*;
 import java.util.*;
@@ -7,31 +7,30 @@ public class halfBirthday{
     
    public static void main(String [] args){
         
-        int currYear;
-        int month;
-        int day;
+        int currYear; //int of current year of birthday ex '2001' for LocalDate birth line 24
+        int month; // int month of birthday ex '01' for LocalDate birth line 24
+        int day; // int day of birthday ex '01' for LocalDate birth line 24
        
        
-       
-        Scanner in = new Scanner (System.in);
+        Scanner in = new Scanner (System.in); //scanner in
         
-        System.out.println("Enter the year your birthday will fall on, followed by month, and day of your birthday: ex. '2018', '01, '01'");
+        System.out.println("Enter the year your birthday will fall on, followed by month, and day of your birthday: ex. '2018', '01, '01'"); //user prompt
         
-        currYear = in.nextInt();
-        month = in.nextInt();
-        day = in.nextInt();
+        currYear = in.nextInt(); //user input for year
+        month = in.nextInt(); //user input for month
+        day = in.nextInt(); //user input for day
         
-        LocalDate birth = LocalDate.of(currYear, month, day);
+        LocalDate birth = LocalDate.of(currYear, month, day); //LocalDate of Birthday
         
-        System.out.println("Your birthday is " + birth + ".");
+        System.out.println("Your birthday is " + birth + "."); //output for birthday
        
-        LocalDate now = LocalDate.now();
+        LocalDate now = LocalDate.now(); //Today's date for reference.
        
-        System.out.println("Today is " + now + ".");
+        System.out.println("Today is " + now + "."); //out for today's date
        
 
-           System.out.println("Your half birthday is " + birth.minusDays(183) + ".");
-           System.out.println("Your half birthday is " + Period.between(birth.minusDays(183), now) + ".");
+           System.out.println("Your half birthday is " + birth.minusDays(183) + "."); //half birthday with algorithm for 183 days
+           System.out.println("Your half birthday is " + Period.between(birth.minusDays(183), now) + "."); //months and days until halfbirthday
     }
     
 }
